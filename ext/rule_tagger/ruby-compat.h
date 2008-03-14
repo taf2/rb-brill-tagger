a71d4177a798fee0aa13e323756ad716482a8bbf
Todd Fisher <taf2@bigbox.localdomain>
2008-03-14 10:40:58 -0400

    make sure we include ruby-compat header

---
+++ ext/rule_tagger/ruby-compat.h
@@ -0,0 +1,20 @@
+#ifndef _RUBY_COMPAT_HEADER_H
+#define _RUBY_COMPAT_HEADER_H
+
+#define DEBUG
+#ifdef DEBUG
+#define TRACE()  fprintf(stderr, "> %s:%d:%s\n", __FILE__, __LINE__, __FUNCTION__)
+#else
+#define TRACE() 
+#endif
+
+/* ruby 1.9 compat */
+#ifndef RSTRING_PTR
+#define RSTRING_PTR(str) RSTRING(str)->ptr
+#endif
+
+#ifndef RSTRING_LEN
+#define RSTRING_LEN(str) RSTRING(str)->len
+#endif
+
+#endif
