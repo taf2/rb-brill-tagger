81264447aa39db03e780e113883c4197b153d73a
Todd Fisher <todd.fisher@gmail.com>
2008-05-13 16:48:58 -0400

    check for snprintf

--- ext/rule_tagger/extconf.rb
+++ ext/rule_tagger/extconf.rb
@@ -1,9 +1,14 @@
 require 'mkmf'
 
-dir_config("rule_tagger")
-have_header("stdlib.h")
-have_header("string.h")
-have_library("c", "main")
+dir_config('rule_tagger')
+have_header('stdlib.h')
+have_header('string.h')
+have_library('c', 'main')
+
+if !have_func('snprintf', 'stdio.h')
+  raise "You must have snprintf available to compile this library"
+end
+
 CFLAGS='-Wall -g'
 
-create_makefile("rule_tagger")
+create_makefile('rule_tagger')
